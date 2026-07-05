5a072414ce03d14f95ff62039b6f4f5b424cccbc
Aamir Raza <syedraza6157@gmail.com>
2026-07-05 08:51:02 +0000

    conversion and operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+let score  = "aamir"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber  = Number(score)
+ 
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber) 
+
+let isLoggedIn = "raza"
+ 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+ 
+// true = 1; false = 0;
+// ""  = false
+// "raza" = true
+
+let anyNumber = 72
+let numberIntoString = String(anyNumber)
+
+console.log(numberIntoString); //72
+console.log(typeof numberIntoString); //will show string  
+
+ //************Operations*********************// 
+ let value = 5
+ let negValue = -value
+// console.log(negValue);
+ 
+ let str1 = "Hello "
+ let str2 = "Raza"
+ let str3 = str1 + str2
+
+// console.log(str3);
+ 
+// console.log(1 + 2); //3
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 3); //123
+let add = 1 + 2 + "3"
+// console.log(add); //"33"
+// console.log(typeof add); //string not number 
+
+ let gameScore = 100
+ ++gameScore
+ console.log(gameScore);
+ 
+
+
+
+
