2081b0da7937b931dfc6e21d78f3971a2c507be8
Aamir Raza <syedraza6157@gmail.com>
2026-07-11 18:18:53 +0000

    Complete JavaScript Objects Part 1

---
+++ 02_basics/03_object.js
@@ -0,0 +1,41 @@
+// Singleton object can be created using Object.create()
+Object.create
+
+// object literals 
+const mySymbol = Symbol("key1")
+
+const user = {
+    name: "aamir",
+    "fullName": "Aamir Raza", //you can't access using dot,access with bracket notation
+    age: 19,
+    location: "Delhi",
+    email: "aamir123@gmail.com",
+    isLoggedIn: false,
+    lastLogIn: ["Monday"],
+
+    // Symbol used as a unique object key
+    [mySymbol]: "mykey1"  
+}
+
+// console.log(user.email);
+// console.log(user["email"]);
+// console.log(user["fullName"]);
+//  console.log(user[mySymbol]);
+
+user.location = "Patna"
+// Object.freeze(user)    // Prevent further modifications
+user.location = "Ranchi"
+// console.log(user);
+
+user.greet = function(){
+    console.log("Hello User");
+    
+}
+user.greetTwo = function(){
+    // Using 'this' to access current object properties
+    console.log(`Hello User,${this.name}`);
+    
+}
+console.log(user.greet());
+console.log(user.greetTwo());
+
