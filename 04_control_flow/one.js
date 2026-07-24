90dd5dc54504ab17c096b8364a786a64a224e5bf
aamirraza72 <syedraza6157@gmail.com>
2026-07-24 23:49:05 +0530

    practice if-else conditions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,59 @@
+
+
+const isUserLoggedIn =  true
+ const temperature = 25
+
+// if(temperature > 45){
+//        console.log("Excessive heat");
+//     }
+//      else{
+//         console.log("Temperature is good");
+        //      }
+
+//   Operators
+
+ // <, >, <=, >=, ==, !=, ===, !==
+
+ const score = 200
+
+//  if(score > 100){
+//     const power = "Fly"
+//     console.log(`User power: ${power}`);
+
+//     }
+//  console.log(`User power: ${power}`);
+
+const balance = 1000
+// if(balance > 500) console.log(`balance: ${balance}`);
+
+// if(balance < 800) {
+//     console.log("Less than 800");
+    
+// }
+// else if(balance < 600){
+//     console.log("Less than 600");
+    
+// }
+// else if(balance < 400){
+//     console.log("Less than 400");
+    
+// }
+// else{
+//     console.log("balance is less 1500");
+    
+// }
+
+const  userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to shopping");
+    
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged In");
+    
+}
