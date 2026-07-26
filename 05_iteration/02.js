61112b354c6ec82c7b466516ea41e832d05cbd14
aamirraza72 <syedraza6157@gmail.com>
2026-07-26 23:40:04 +0530

    learn: while and do-while

---
+++ 05_iteration/02.js
@@ -0,0 +1,21 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+let myArray = ["ChatGPT", "Gemini", "Claude"]
+ 
+let arrayIndex = 0
+ while (arrayIndex < myArray.length) {
+   // console.log(`Value is ${myArray[arrayIndex]}`);
+    arrayIndex++
+    
+ }
+
+ let score = 1
+ do {
+    console.log(`Score is ${score}`);
+    score++
+ } while (score <= 10);
