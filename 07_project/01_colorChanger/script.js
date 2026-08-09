4cfb83e9d4bfbc878b85faf1a1428045d56958f0
aamirraza72 <syedraza6157@gmail.com>
2026-08-09 21:57:17 +0530

    JavaScript

--- 07_project/01_colorChanger/script.js
+++ 07_project/01_colorChanger/script.js
@@ -1,10 +1,22 @@
 const buttons = document.querySelectorAll('.color-btn')
 // console.log(buttons)
 const body = document.querySelector("body")
+const homeLink = document.querySelector('#homeLink')
 
 const currentColor = document.querySelector('#currentColor')
 let count = 0
 const countElement = document.querySelector('#count')
+
+const resetBtn = document.querySelector('#resetbtn')
+
+homeLink.addEventListener('click', function (event) {
+  event.preventDefault()
+})
+
+resetBtn.addEventListener('click', (e) => {
+    body.style.backgroundColor = 'white';
+    currentColor.textContent = 'White';
+});
 
 buttons.forEach( function (button){
   // console.log(button)
